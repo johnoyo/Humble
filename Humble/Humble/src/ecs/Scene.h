1e91e1cbf6c932150d04addf8cf9f2682477a3f1
John Petropoulos <johnaipeia@gmail.com>
2022-06-05 18:30:03 +0300

    - Renamed scene functions.

--- Humble/Humble/src/ecs/Scene.h
+++ Humble/Humble/src/ecs/Scene.h
@@ -4,9 +4,9 @@ namespace HBL {
 
 	class Scene {
 	public:
-		virtual void Enroll() = 0;
-		virtual void Add() = 0;
-		virtual void Init() = 0;
+		virtual void Enroll_Entities() = 0;
+		virtual void Add_Components() = 0;
+		virtual void Init_Components() = 0;
 
 		void Set_Level(const std::string& level) { _level = level; }
 		std::string& Get_Level() { return _level; }
